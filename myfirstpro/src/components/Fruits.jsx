3936801c64702b56e60cce25c86e375efc643f9d
unknown <adiprasetyaxyz@icloud.com>
2024-06-24 12:50:29 +0800

    15 : Rendering Components Inside A Loop

--- myfirstpro/src/components/Fruits.jsx
+++ myfirstpro/src/components/Fruits.jsx
@@ -1,19 +1,23 @@
+import Fruit from "./Fruit";
+
 export default function Fruits() {
-    // const fruits = ["Apple", "Banana", "Orange", "Pineaple"]
-    const fruits = [
-        {name: "Apple", price: 10000},
-        {name: "Mango", price: 11000},
-        {name: "Orange", price: 9000},
-        {name: "Pineapple", price: 13000},
-    ]
-    return (
+  // const fruits = ["Apple", "Banana", "Orange", "Pineaple"]
+  const fruits = [
+    { name: "Apple", price: 10000 },
+    { name: "Mango", price: 11000 },
+    { name: "Orange", price: 9000 },
+    { name: "Pineapple", price: 13000 },
+  ];
+  return (
     <div>
-    <ul>
+      <ul>
         {fruits.map((fruit) => (
-            <li key={fruit.name}>
-                {fruit.name} : Rp.{fruit.price}
-                </li>
+          // <li key={fruit.name}>
+          //     {fruit.name} : Rp.{fruit.price}
+          //     </li>
+          <Fruit key={fruit.name} name={fruit.name} price={fruit.price} />
         ))}
-    </ul>
-    </div>)
+      </ul>
+    </div>
+  );
 }
