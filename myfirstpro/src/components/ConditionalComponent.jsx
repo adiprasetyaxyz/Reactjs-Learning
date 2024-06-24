fa6fce64970fc2fa4f750903db73276e085b43c2
unknown <adiprasetyaxyz@icloud.com>
2024-06-24 13:56:42 +0800

    16 : Conditionally Rendering JSX & Components

--- myfirstpro/src/components/ConditionalComponent.jsx
+++ myfirstpro/src/components/ConditionalComponent.jsx
@@ -4,10 +4,5 @@ import Welcome from "./Welcome";
 export default function Conditional() {
   const display = true;
   let message = "";
-  if (display) {
-    message = <h1>Message 1</h1>;
-  } else {
-    message = <h1>Message 2</h1>;
-  }
-  return message;
+  return display ? <Welcome /> : <Code />;
 }
